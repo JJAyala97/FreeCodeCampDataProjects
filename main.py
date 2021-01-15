d2d6151d17b8ce554ba82bdd520ee93413ac61cf
JJAyala97 <68266522+JJAyala97@users.noreply.github.com>
2021-01-14 23:00:53 -0500

    Adding try except for if anything other than a number is put in the list

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import numpy as np
 
 def calculate(list):
-  if len(list) != 9:
-    raise ValueError ('List must contain nine numbers.')
-  else:
-    newlist = np.array(list).reshape(3,3)
-    stats = { 'mean': [np.mean(newlist, axis=0).tolist(), np.mean(newlist, axis=1).tolist(), np.mean(newlist)],
-              'variance': [np.var(newlist, axis=0).tolist(), np.var(newlist, axis=1).tolist(), np.var(newlist)],
-              'standard deviation': [np.std(newlist, axis=0).tolist(), np.std(newlist, axis=1).tolist(), np.std(newlist)],
-              'max': [np.max(newlist, axis=0).tolist(), np.max(newlist, axis=1).tolist(), np.max(newlist)],
-              'min': [np.min(newlist, axis=0).tolist(), np.min(newlist, axis=1).tolist(), np.min(newlist)],
-              'sum': [np.sum(newlist, axis=0).tolist(), np.sum(newlist, axis=1).tolist(), np.sum(newlist)]
-              }
-    for key, value in stats.items():
-      print(key, ':', value)
+#Checking that list has exactly 9 numbers
+  try:
+      if len(list) != 9:
+        raise ValueError ('List must contain nine numbers.')
+      else:
+        newlist = np.array(list).reshape(3,3)
+        stats = { 'mean': [np.mean(newlist, axis=0).tolist(), np.mean(newlist, axis=1).tolist(), np.mean(newlist)],
+                  'variance': [np.var(newlist, axis=0).tolist(), np.var(newlist, axis=1).tolist(), np.var(newlist)],
+                  'standard deviation': [np.std(newlist, axis=0).tolist(), np.std(newlist, axis=1).tolist(), np.std(newlist)],
+                  'max': [np.max(newlist, axis=0).tolist(), np.max(newlist, axis=1).tolist(), np.max(newlist)],
+                  'min': [np.min(newlist, axis=0).tolist(), np.min(newlist, axis=1).tolist(), np.min(newlist)],
+                  'sum': [np.sum(newlist, axis=0).tolist(), np.sum(newlist, axis=1).tolist(), np.sum(newlist)]
+                  }
+        for key, value in stats.items():
+          print(key, ':', value)
+  except TypeError:
+      print("Something went wrong. Please input 9 numbers")
 
 
 
-calculate([0,1,2,3,4,5,6,7,9])
+calculate([1.2,1,2,3,4,5,6,7,9])
 
 
